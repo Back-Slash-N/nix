eb9b7468113b2dde5483178a93e3b5b52bf6cf3c
BackSlashN <thewoodster333@gmail.com>
2025-09-02 15:33:20 -0400

    update & optimize system

--- flake.nix
+++ flake.nix
@@ -3,21 +3,36 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    hyprland.url = "github:hyprwm/Hyprland";
-    millennium.url = "git+https://github.com/SteamClientHomebrew/Millennium?ref=next";
+
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+
+    hyprland = {
+      url = "github:hyprwm/Hyprland";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    millennium = {
+      url = "git+https://github.com/SteamClientHomebrew/Millennium?ref=next";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     home-manager = {
-       url = "github:nix-community/home-manager";
-       inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+
     nixcord = {
-        url = "github:kaylorben/nixcord";
-        inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:kaylorben/nixcord";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+
     firefox-addons = {
       url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     sops-nix = {
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
