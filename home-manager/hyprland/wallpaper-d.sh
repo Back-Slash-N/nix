0a37ab17bfae07bd0e77970e75d31517d7d604a2
BackSlashN <thewoodster333@gmail.com>
2025-09-05 15:13:53 -0400

    remove daze from wallpaper cycle

--- home-manager/hyprland/wallpaper-d.sh
+++ home-manager/hyprland/wallpaper-d.sh
@@ -2,8 +2,7 @@
 # WALLPAPER MANAGER
 while :; do
   if [[ $(date +%T) < 19:00:00 ]] || [[ $(date +%T) > 6:00:00 ]] && [[ $(date +%T) > 0:59:59 ]]; then # before 7 PM and after 6 AM (daytime)
-    mpvpaper --mpv-options "--loop-file=300" ALL ~/.wallpapers/spill-animated.mp4
-    mpvpaper --mpv-options "--loop-file=120" ALL ~/.wallpapers/ila-animated.mp4
+    mpvpaper --mpv-options "--loop-file=1000" ALL ~/.wallpapers/spill-animated.mp4
   elif [[ $(date +%T) > 19:00:00 ]] && [[ $(date +%T) < 23:00:00 ]]; then # after 7 PM and before 11pm
     mpvpaper --mpv-options "--loop-file=500" ALL ~/.wallpapers/overthrower-animated.mp4
   else # 11:00 PM -> 5:59 AM
