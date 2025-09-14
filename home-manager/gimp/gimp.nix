ec13b46f4baabbd42ec0798d0918f421b951136b
BackSlashN <thewoodster333@gmail.com>
2025-09-14 17:08:14 -0400

    remove photogimp

--- home-manager/gimp/gimp.nix
+++ home-manager/gimp/gimp.nix
@@ -1,12 +1,6 @@
 { config, pkgs, ... }:
-let
-  photogimp = pkgs.callPackage ./PhotoGIMP.nix { };
-in {
+{
 
   home.packages = with pkgs; [gimp];
   
-  home.file.".config/GIMP/3.0" = {
-    source = photogimp;
-    recursive = true; # This is optional
-  };
 }
