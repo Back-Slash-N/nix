df8e1f7ec95f5e1664fe37bc6ca183a1ab626813
BackSlashN <thewoodster333@gmail.com>
2025-09-20 14:05:17 -0400

    remove temp patch and update

--- configuration.nix
+++ configuration.nix
@@ -17,20 +17,6 @@
       ./system-configuration/sops/sops.nix
     ];
 
-  #! TEMP FIX: KERNEL REGRESSION CAUSES TAILSCALE BUILD TO FAIL.
-  boot.kernelPatches = [
-    # Fix the /proc/net/tcp seek issue
-    # Impacts tailscale: https://github.com/tailscale/tailscale/issues/16966
-    {
-      name = "proc: fix missing pde_set_flags() for net proc files";
-      patch = pkgs.fetchurl {
-        name = "fix-missing-pde_set_flags-for-net-proc-files.patch";
-        url = "https://patchwork.kernel.org/project/linux-fsdevel/patch/20250821105806.1453833-1-wangzijie1@honor.com/raw/";
-        hash = "sha256-DbQ8FiRj65B28zP0xxg6LvW5ocEH8AHOqaRbYZOTDXg=";
-      };
-    }
-  ];
-
   # Shell
   programs.zsh.enable = true;
   users.defaultUserShell = pkgs.zsh;
